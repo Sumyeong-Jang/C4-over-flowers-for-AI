e7c0ff90978f115c47a152ce24c094dc99c7c9b8
Sumyeong-Jang <dsaewq1101@naver.com>
2022-12-17 22:36:28 +0900

    🎨 Change Mapping param

--- turtlebot3_ws/src/turtlebot3/turtlebot3_cartographer/config/turtlebot3_lds_2d.lua
+++ turtlebot3_ws/src/turtlebot3/turtlebot3_cartographer/config/turtlebot3_lds_2d.lua
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5e9619c353726157b30dd0e3fdbc6dc1fd134494f19ab2560979af309030de4b
-size 1983
+oid sha256:575fcc16e152ccca329084ed5d6002d385c60092192d1d82a8c57602ae126784
+size 1982
